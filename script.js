21e6aee7338ec5fdea69c17944b5097bcc4b24b2
shaikhatk <tk.shaikha@gmail.com>
2023-09-13 15:48:56 +0530

    script updates event trigger

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let liItem = document.querySelectorAll('ul li');
 let imgItem = document.querySelectorAll('.product img');
 
+liItem.forEach((li) => {
+    liItem.addEventListener('click', function () {
+        const value = liItem.getAttribute('data-filter');
+
+        imgItem.forEach(img => {
+            if (value === "allImages" || imgItem.classList.contains(value)) {
+                imgItem.style.display = "block";
+            } else {
+                imgItem.style.display = "none";
+            }
+        });
+    });
+});
+
+
